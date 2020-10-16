64d14843bc9ffcb2d9137873660dbbe9dee90918
rin.ogawa <wanoku01@yahoo.co.jp>
2020-10-16 11:37:52 +0000

    kadai login function modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,9 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy, :new]
 
   def index
-    if logged_in?
-      @task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    else
-      redirect_to 'session/new'
-    end
   end
 
   def show
@@ -59,4 +55,12 @@ class TasksController < ApplicationController
   def task_params
       params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
+
